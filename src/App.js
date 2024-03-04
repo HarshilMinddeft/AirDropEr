c8f958930f01a109483b8c46ef13366140bf13a1
HarshilMinddeft <harshil.mandali@minddeft.net>
2024-03-04 18:47:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import KECCAK256 from "keccak256";
+import MerkleTree from "merkletreejs";
+import { Buffer } from "buffer/";
+
+import "./App.css";
+
+import artifact from "./artifacts/contracts/MerkleDistributor.sol/MerkleDistributor.json";
+const CONTRACT_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+
+window.Buffer = window.Buffer || Buffer;
 
 function App() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [signerAddress, setSignerAddress] = useState(undefined);
+  const [tree, setTree] = useState(undefined);
+  const [proof, setProof] = useState([]);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.BrowserProvider(window.ethereum);
+      setProvider(provider);
+
+      const contract = await new ethers.Contract(
+        CONTRACT_ADDRESS,
+        artifact.abi,
+        provider
+      );
+      setContract(contract);
+
+      const tree = await getTree();
+      setTree(tree);
+    };
+    onLoad();
+  }, []);
+
+  const isConnected = () => signer !== undefined;
+
+  const connect = () => {
+    getSigner(provider).then((signer) => {
+      setSigner(signer);
+    });
+  };
+
+  const getTree = async () => {
+    const indexedAddresses = require("./walletAddresses.json");
+
+    const addresses = [];
+    Object.keys(indexedAddresses).forEach(function (idx) {
+      addresses.push(indexedAddresses[idx]);
+    });
+
+    const leaves = addresses.map((x) => KECCAK256(x));
+    const tree = new MerkleTree(leaves, KECCAK256, { sortPairs: true });
+
+    return tree;
+  };
+
+  const getSigner = async (provider) => {
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+
+    setSignerAddress(address);
+
+    const proof = tree.getHexProof(KECCAK256(address));
+    setProof(proof);
+
+    return signer;
+  };
+
+  const claimAirdrop = async () => {
+    await contract.connect(signer).claim(proof);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isConnected() ? (
+          <div>
+            <p>Welcome {signerAddress?.substring(0, 10)}...</p>
+            <div className="list-group">
+              <div className="list-group-item">
+                <button
+                  className="btn btn-success"
+                  onClick={() => claimAirdrop()}
+                >
+                  Claim
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>You are not connected</p>
+            <button onClick={connect} className="btn btn-primary">
+              Connect Metamask
+            </button>
+          </div>
+        )}
       </header>
     </div>
   );
